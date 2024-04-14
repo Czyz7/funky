5883caed5d020165d949237f983b9859a4fac0d5
Czyz7 <czyzhernandez@outlook.com>
2024-04-13 21:19:48 -0700

    portfolio attempt.

--- src/app/sitepages/portfolio/page.tsx
+++ src/app/sitepages/portfolio/page.tsx
@@ -1,10 +1,11 @@
+import React, { Fragment } from 'react';
 import Navigation from '@/components/Navigation';
 import Block from '@/components/block';
 import Head from 'next/head';
 
 const Portfolio = () => {
     return (
-        <>
+        <Fragment>
             <Head>
                 <title>SEO Page | Accessible Web Designs</title>
                 <meta name="description" content="This is a filler page for the SEO page." />
@@ -12,13 +13,10 @@ const Portfolio = () => {
             <Navigation />
             <Block />
 
+
             <main className="bg-gradient-to-r from-emerald-400 to-cyan-400 text-black py-20">
-                <h1>SEO Page</h1>
-                <p>
-                    This is a filler page for the Portfolio page.
-                </p>
             </main>
-        </>
+        </Fragment>
     );
 };
 
