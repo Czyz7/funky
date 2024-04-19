cce8cad1047607081fd7bed2cbeb94b9058f1ea1
Czyz7 <czyzhernandez@outlook.com>
2024-04-19 15:01:11 -0700

    Completed ServiceGallery component.

--- src/app/sitepages/seo/page.tsx
+++ src/app/sitepages/seo/page.tsx
@@ -2,10 +2,8 @@ import React from 'react';
 import Navigation from '@/components/Navigation';
 import Block from '@/components/block';
 import Head from 'next/head';
-import SeoKey from '@/components/ServiceForms/SeoKey';
-import SeoOnin from '@/components/ServiceForms/SeoOnin';
 import HeroSeo from '@/components/Hero/HeroSeo';
-import SeoTechn from '@/components/ServiceForms/SeoTechn';
+import SeoServ from '@/components/ServiceForms/SeoServ';
 
 const SEO = () => {
 
@@ -21,13 +19,12 @@ const SEO = () => {
             <HeroSeo />
 
             <main className="bg-gradient-to-r from-emerald-400 to-cyan-400 text-black py-20">
-                <h1 className="sm:text-4xl text-3xl font-medium title-font text-center mb-2 text-gray-900">Pricing for Technical SEO Services</h1>
+
                 <Block />
-                <SeoKey />
-                <Block />
+
                 <h1 className="sm:text-4xl text-3xl font-medium title-font text-center mb-2 text-gray-900">Pricing for Keyword Research Services</h1>
                 <Block />
-                <SeoTechn />
+                <SeoServ />
             </main>
         </>
     );
--- src/components/ServiceGallery.tsx
+++ src/components/ServiceGallery.tsx
@@ -5,17 +5,18 @@ const ServiceGallery = () => {
     return (
         <section className="text-gray-600 body-font">
             {/* ... (rest of your component with the changes below) ... */}
-            <div className="container px-5 md:px-10 lg:px-6 py-8 rounded-3xl bg-gray-800 mx-auto">
+            <div className=" px-5 md:px-10 lg:px-6 py-8 rounded-3xl bg-gray-800 ">
                 <div className="flex flex-wrap -m-4">
-                    <div className="xl:w-1/4 md:w-1/2 p-3 rounded-lg transition duration-300 ease-in-out hover:scale-110 ">
-                        <Link href="/sitepages/blogpost">
-                            <div className="p-24 rounded-lg relative"> {/* Make the container relative */}
+                    <div className="xl:w-1/4 md:w-1/2 p-20 rounded-lg opacity-75 transform transition duration-500 ease-in-out hover:opacity-100 hover:bg-indigo-500">
+                        <Link href="/sitepages/coding">
+                            <div className="p-20 rounded-lg bg-cover bg-center "> {/* Make the container relative */}
                                 <Image
-                                    src="/coolGuy.png"
+                                    src="/Hero Images/compMstr.png"
                                     alt="Image Description"
                                     layout="fill"  // Fill the container
                                     objectFit="cover" // Adjust how the image fits
                                     quality={100}
+                                    className='absolute inset-0'
                                     priority={true} // Prioritize this image for loading (optional)
                                 />
                                 <div className="absolute inset-0 flex flex-col items-center justify-center w-full h-full">
@@ -28,77 +29,146 @@ const ServiceGallery = () => {
                     </div>
 
                     {/* ... (similar changes to the rest of your divs) ... */}
-                    <div className="xl:w-1/4 md:w-1/2 p-3 rounded-lg transition duration-300 ease-in-out hover:scale-110 ">
-                        <Link href="/sitepages/blogpost">
-                            {/* Use your custom class or inline background url */}
-                            <div className="p-24 rounded-lg bg-blogpost-bg bg-cover bg-center">
-                                <div className="flex flex-col items-center justify-center w-full h-full">
+                    <div className="xl:w-1/4 md:w-1/2 p-20 rounded-lg opacity-75 transform transition duration-500 ease-in-out hover:opacity-100 hover:bg-indigo-500">
+                        <Link href="/sitepages/wordpress">
+                            <div className="p-20 rounded-lg bg-center "> {/* Make the container relative */}
+                                <Image
+                                    src="/Hero Images/wpEr.png"
+                                    alt="Image Description"
+                                    layout="fill"  // Fill the container
+                                    objectFit="cover" // Adjust how the image fits
+                                    quality={100}
+                                    className='absolute inset-0'
+                                    priority={true} // Prioritize this image for loading (optional)
+                                />
+                                <div className="absolute inset-0 flex flex-col items-center justify-center w-full h-full">
+                                    {/* Overlay your content on top of the image */}
                                     <h3 className="tracking-widest text-indigo-500 text-sm font-medium title-font">AI Assisted</h3>
-                                    <h2 className="text-lg text-indigo-50 font-normal title-font mb-2 ">WordPress Design</h2>
+                                    <h2 className="text-lg text-indigo-50 font-medium title-font mb-2 ">WordPress Development</h2>
                                 </div>
                             </div>
                         </Link>
                     </div>
-                    <div className="xl:w-1/4 md:w-1/2 p-3 rounded-lg transition duration-300 ease-in-out hover:scale-110 ">
-                        <Link href="/sitepages/blogpost">
-                            {/* Use your custom class or inline background url */}
-                            <div className="p-24 rounded-lg bg-blogpost-bg bg-cover bg-center">
-                                <div className="flex flex-col items-center justify-center w-full h-full">
+
+                    <div className="xl:w-1/4 md:w-1/2 p-20 rounded-lg opacity-75 transform transition duration-500 ease-in-out hover:opacity-100 hover:bg-indigo-500">
+                        <Link href="/sitepages/manage">
+                            <div className="p-20 rounded-lg bg-cover bg-center "> {/* Make the container relative */}
+                                <Image
+                                    src="/Hero Images/officeCollab.png"
+                                    alt="Image Description"
+                                    layout="fill"  // Fill the container
+                                    objectFit="cover" // Adjust how the image fits
+                                    quality={100}
+                                    className='absolute inset-0'
+                                    priority={true} // Prioritize this image for loading (optional)
+                                />
+                                <div className="absolute inset-0 flex flex-col items-center justify-center w-full h-full">
+                                    {/* Overlay your content on top of the image */}
                                     <h3 className="tracking-widest text-indigo-500 text-sm font-medium title-font">AI Assisted</h3>
-                                    <h2 className="text-lg text-indigo-50 font-medium title-font mb-2 ">Site Management</h2>
+                                    <h2 className="text-lg text-indigo-50 font-medium title-font mb-2 ">Website Management</h2>
                                 </div>
                             </div>
                         </Link>
                     </div>
-                    <div className="xl:w-1/4 md:w-1/2 p-3 rounded-lg transition duration-300 ease-in-out hover:scale-110 ">
-                        <Link href="/sitepages/blogpost">
-                            {/* Use your custom class or inline background url */}
-                            <div className="p-24 rounded-lg bg-blogpost-bg bg-cover bg-center">
-                                <div className="flex flex-col items-center justify-center w-full h-full">
+
+                    <div className="xl:w-1/4 md:w-1/2 p-20 rounded-lg opacity-75 transform transition duration-500 ease-in-out hover:opacity-100 hover:bg-indigo-500">
+                        <Link href="/sitepages/redesign">
+                            <div className="p-20 rounded-lg bg-cover bg-center "> {/* Make the container relative */}
+                                <Image
+                                    src="/Hero Images/web0.png"
+                                    alt="Image Description"
+                                    layout="fill"  // Fill the container
+                                    objectFit="cover" // Adjust how the image fits
+                                    quality={100}
+                                    className='absolute inset-0'
+                                    priority={true} // Prioritize this image for loading (optional)
+                                />
+                                <div className="absolute inset-0 flex flex-col items-center justify-center w-full h-full">
+                                    {/* Overlay your content on top of the image */}
                                     <h3 className="tracking-widest text-indigo-500 text-sm font-medium title-font">AI Assisted</h3>
                                     <h2 className="text-lg text-indigo-50 font-medium title-font mb-2 ">Website Redesign</h2>
                                 </div>
                             </div>
                         </Link>
                     </div>
-                    <div className="xl:w-1/4 md:w-1/2 p-3 rounded-lg transition duration-300 ease-in-out hover:scale-110 ">
+
+                    <div className="xl:w-1/4 md:w-1/2 p-20 rounded-lg opacity-75 transform transition duration-500 ease-in-out hover:opacity-100 hover:bg-indigo-500">
                         <Link href="/sitepages/blogpost">
-                            {/* Use your custom class or inline background url */}
-                            <div className="p-24 rounded-lg bg-blogpost-bg bg-cover bg-center">
-                                <div className="flex flex-col items-center justify-center w-full h-full">
+                            <div className="p-20 rounded-lg bg-cover bg-center "> {/* Make the container relative */}
+                                <Image
+                                    src="/Hero Images/techDude1.png"
+                                    alt="Image Description"
+                                    layout="fill"  // Fill the container
+                                    objectFit="cover" // Adjust how the image fits
+                                    quality={100}
+                                    className='absolute inset-0'
+                                    priority={true} // Prioritize this image for loading (optional)
+                                />
+                                <div className="absolute inset-0 flex flex-col items-center justify-center w-full h-full">
+                                    {/* Overlay your content on top of the image */}
                                     <h3 className="tracking-widest text-indigo-500 text-sm font-medium title-font">AI Assisted</h3>
-                                    <h2 className="text-lg text-indigo-50 font-medium title-font mb-2 ">Blog Posts</h2>
+                                    <h2 className="text-lg text-indigo-50 font-medium title-font mb-2 ">Blog Post Generation</h2>
                                 </div>
                             </div>
                         </Link>
                     </div>
-                    <div className="xl:w-1/4 md:w-1/2 p-3 rounded-lg transition duration-300 ease-in-out hover:scale-110 ">
-                        <Link href="/sitepages/blogpost">
-                            {/* Use your custom class or inline background url */}
-                            <div className="p-24 rounded-lg bg-blogpost-bg bg-cover bg-center">
-                                <div className="flex flex-col items-center justify-center w-full h-full">
+
+                    <div className="xl:w-1/4 md:w-1/2 p-20 rounded-lg opacity-75 transform transition duration-500 ease-in-out hover:opacity-100 hover:bg-indigo-500">
+                        <Link href="/sitepages/seo">
+                            <div className="p-20 rounded-lg bg-cover bg-center "> {/* Make the container relative */}
+                                <Image
+                                    src="/Hero Images/seo0.png"
+                                    alt="Image Description"
+                                    layout="fill"  // Fill the container
+                                    objectFit="cover" // Adjust how the image fits
+                                    quality={100}
+                                    className='absolute inset-0'
+                                    priority={true} // Prioritize this image for loading (optional)
+                                />
+                                <div className="absolute inset-0 flex flex-col items-center justify-center w-full h-full">
+                                    {/* Overlay your content on top of the image */}
                                     <h3 className="tracking-widest text-indigo-500 text-sm font-medium title-font">AI Assisted</h3>
                                     <h2 className="text-lg text-indigo-50 font-medium title-font mb-2 ">SEO</h2>
                                 </div>
                             </div>
                         </Link>
                     </div>
-                    <div className="xl:w-1/4 md:w-1/2 p-3 rounded-lg transition duration-300 ease-in-out hover:scale-110 ">
-                        <Link href="/sitepages/blogpost">
-                            {/* Use your custom class or inline background url */}
-                            <div className="p-24 rounded-lg bg-blogpost-bg bg-cover bg-center">
-                                <div className="flex flex-col items-center justify-center w-full h-full">
+
+                    <div className="xl:w-1/4 md:w-1/2 p-20 rounded-lg opacity-75 transform transition duration-500 ease-in-out hover:opacity-100 hover:bg-indigo-500">
+                        <Link href="/sitepages/accessibility">
+                            <div className="p-20 rounded-lg bg-cover bg-center "> {/* Make the container relative */}
+                                <Image
+                                    src="/Hero Images/compMstr.png"
+                                    alt="Image Description"
+                                    layout="fill"  // Fill the container
+                                    objectFit="cover" // Adjust how the image fits
+                                    quality={100}
+                                    className='absolute inset-0'
+                                    priority={true} // Prioritize this image for loading (optional)
+                                />
+                                <div className="absolute inset-0 flex flex-col items-center justify-center w-full h-full">
+                                    {/* Overlay your content on top of the image */}
                                     <h3 className="tracking-widest text-indigo-500 text-sm font-medium title-font">AI Assisted</h3>
                                     <h2 className="text-lg text-indigo-50 font-medium title-font mb-2 ">Accessibility</h2>
                                 </div>
                             </div>
                         </Link>
                     </div>
-                    <div className="xl:w-1/4 md:w-1/2 p-3 rounded-lg transition duration-300 ease-in-out hover:scale-110 ">
-                        <Link href="/sitepages/blogpost">
-                            {/* Use your custom class or inline background url */}
-                            <div className="p-24 rounded-lg bg-blogpost-bg bg-cover bg-center">
-                                <div className="flex flex-col items-center justify-center w-full h-full">
+
+                    <div className="xl:w-1/4 md:w-1/2 p-20 rounded-lg opacity-75 transform transition duration-500 ease-in-out hover:opacity-100 hover:bg-indigo-500">
+                        <Link href="/sitepages/consult">
+                            <div className="p-20 rounded-lg bg-cover bg-center "> {/* Make the container relative */}
+                                <Image
+                                    src="/Hero Images/compMstr.png"
+                                    alt="Image Description"
+                                    layout="fill"  // Fill the container
+                                    objectFit="cover" // Adjust how the image fits
+                                    quality={100}
+                                    className='absolute inset-0'
+                                    priority={true} // Prioritize this image for loading (optional)
+                                />
+                                <div className="absolute inset-0 flex flex-col items-center justify-center w-full h-full">
+                                    {/* Overlay your content on top of the image */}
                                     <h3 className="tracking-widest text-indigo-500 text-sm font-medium title-font">AI Assisted</h3>
                                     <h2 className="text-lg text-indigo-50 font-medium title-font mb-2 ">Consulting</h2>
                                 </div>
